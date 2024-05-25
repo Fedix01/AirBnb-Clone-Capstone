f290de78db8efb87fbc1c945a13d8105840d34f8
Federico Manna <federicomanna01@yahoo.it>
2024-05-25 16:45:23 +0200

    add photos in details page

--- front-end/src/components/InsertionDetailsPage/InsertionDetailsPage.jsx
+++ front-end/src/components/InsertionDetailsPage/InsertionDetailsPage.jsx
@@ -3,6 +3,8 @@ import { FaRegHeart } from "react-icons/fa6";
 import { IoShareOutline } from "react-icons/io5";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import emptyLocation from "../../assets/empty.png";
+
 
 export default function InsertionDetailsPage(props) {
 
@@ -28,12 +30,35 @@ export default function InsertionDetailsPage(props) {
             </div>
             <Row>
                 <Col md={6}>
-                    <img src={covers[0]}
+                    <img src={covers[0] ? covers[0] : emptyLocation}
+                        className='img-fluid'
                         alt=""
-                        height={"300px"} />
-                </Col>
-                <Col>
 
+                        style={{
+                            borderTopLeftRadius: "15px",
+                            borderBottomLeftRadius: "15px",
+                            minHeight: "440px"
+                        }}
+                    />
+                </Col>
+                <Col md={6}>
+                    <div className='d-flex mb-2'>
+                        <img src={covers[1] ? covers[1] : emptyLocation} className='me-2'
+                            alt=""
+                            style={{ height: "210px", width: "270px" }} />
+                        <img src={covers[2] ? covers[2] : emptyLocation}
+                            alt=""
+                            style={{ height: "210px", width: "270px", borderTopRightRadius: "15px" }} />
+                    </div>
+                    <div className='d-flex'>
+                        <img src={covers[3] ? covers[3] : emptyLocation}
+                            className='me-2'
+                            style={{ height: "210px", width: "270px" }}
+                            alt="" />
+                        <img src={covers[4] ? covers[4] : emptyLocation}
+                            style={{ height: "210px", width: "270px", borderBottomRightRadius: "15px" }}
+                            alt="" />
+                    </div>
                 </Col>
             </Row>
         </div>
